3401c22cc27062aee8fdb879ef699dfe92e99b9a
Ivo Lukac <ivo@netgen.hr>
2015-07-18 12:32:50 +0200

    single installation script

---
+++ installation/run.sh
@@ -0,0 +1,15 @@
+mysql -usummer -pcamp -e "create database ezsylius character set utf8"
+mysql ezsylius -usummer -pcamp < installation/db.dmp
+php composer.phar install --no-dev -n
+
+cp -R installation/ezpublish_legacy/settings/override ezpublish_legacy/settings/
+cp -R installation/ezpublish_legacy/settings/siteaccess/administration ezpublish_legacy/settings/siteaccess/
+cp -R installation/ezpublish_legacy/settings/siteaccess/eng ezpublish_legacy/settings/siteaccess/
+cp -R installation/ezpublish_legacy/var/autoload ezpublish_legacy/var/
+cp -R installation/ezpublish_legacy/var/ezdemo_site ezpublish_legacy/var/
+
+php ezpublish/console assets:install --symlink --relative
+php ezpublish/console assetic:dump
+php ezpublish/console assetic:dump --env=prod web
+php ezpublish/console cache:clear --no-warmup
+
